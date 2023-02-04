5ba3c8b93338ccd6dc72b0c40191ac339c9008d4
Alessandro Ciffo <diogene.ciffo@gmail.com>
2023-02-04 16:27:28 +0100

    added about

--- src/About.js
+++ src/About.js
@@ -1,7 +1,56 @@
 import React, {  useState } from "react";
+import Box from '@mui/material/Box';
+import './App.css';
+
+import logo from './assets/SuggestABook.png';
 
 function About(){
-    return (<>hello</>);
+    const [response, setResponse] = useState("");
+    const [textValue, setText] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const styles = {
+        backgroundColor: '#F0F0F0',
+        width: '50%',
+        padding: '20px',
+        margin: 'auto',
+        textAlign: 'left',
+        borderRadius: '25px'
+      };
+
+    const italicStyles = {
+        fontStyle: 'italic',
+    };
+
+    return (
+
+        <div className="App">
+            <Box
+                component="img"
+                sx={{
+                marginTop: "20px",
+                maxHeight: { xs: 80, md: 161 },
+                maxWidth: { xs: 200, md: 400 },
+                }}
+                alt="SugestABookLogo."
+                src={logo}
+            />
+            <div style={styles}>
+            <h3>
+                About
+            </h3>
+            <p>
+                We use GPT-3.5 text generation model to create personalized book suggestions.
+            </p>
+            <p>
+                To get a list of recommendations, just enter your prompt (e.g. <span style={italicStyles}>“suggest me a sci-fi book with AI characters”</span>)
+            </p>
+            <p>
+                The website is inspired by the subreddit  r/suggestmeabook
+            </p>
+            </div>
+        </div>
+    )
 }
 
 export default About
